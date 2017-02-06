ff31e0ffa98806cb94d02fb6fb87a1aded457044
Julien Richard-Foy <julien@richard-foy.fr>
2017-02-06 14:22:19 +0100

    Overloading based implementation of Set and SortedSet

--- src/main/scala/strawman/collection/Set.scala
+++ src/main/scala/strawman/collection/Set.scala
@@ -1,4 +1,27 @@
 package strawman
 package collection
 
-trait Set[A] extends Iterable[A] {}
+import scala.Boolean
+
+/** Base trait for set collections */
+trait Set[A]
+  extends Iterable[A]
+    with SetLike[A, Set]
+
+/** Base trait for set operations */
+trait SetLike[A, +C[X] <: Set[X]]
+  extends IterableLike[A, C]
+    with SetMonoTransforms[A, C[A]] {
+
+  def contains(elem: A): Boolean
+
+}
+
+/** Monomorphic transformation operations */
+trait SetMonoTransforms[A, +Repr] {
+
+  def & (that: Set[A]): Repr
+
+  def ++ (that: Set[A]): Repr
+
+}
